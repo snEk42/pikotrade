6baa0d57f91bab585a5e2b145f8bcfcc4d95e4e7
Jiri Erhart <jiri.erhart@strv.com>
2017-11-07 00:30:17 +0100

    test weighting function change

--- server/utils/math.js
+++ server/utils/math.js
@@ -27,8 +27,8 @@ const calculateNewExchangeRates = (order, commodities) => {
   }
   return commodities.map(commodity => {
     const newValue = commodity.id === order.commodityId
-      ? commodity.value + (koeficient * order.amount)
-      : commodity.value - ((koeficient * order.amount / 3) * getCommodityWeight(order.commodityId))
+      ? commodity.value + ((koeficient * order.amount) / getCommodityWeight(order.commodityId))
+      : commodity.value - ((koeficient * order.amount / 3) / getCommodityWeight(order.commodityId))
     return {
       commodityId: commodity.id,
       index: commodity.index + 1,
