c39c977d4cd1d4eb98f4681b682d85aa6502c41c
Jiri Erhart <jiri.erhart@strv.com>
2017-11-08 08:01:19 +0100

    Adjust for poor resolutions

--- scripts/loadTest.js
+++ scripts/loadTest.js
@@ -49,7 +49,7 @@ async function requestGenerator() {
       .then(body => {
         console.log(body)
       })
-    await Promise.delay(200)
+    await Promise.delay(2000)
   }
 } // .then(() => console.log('Done'))
 
